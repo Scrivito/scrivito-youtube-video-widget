c8d252d1ee57791033ce7ba48776d1b41cdb1dc4
Alexander Pepper <alexander.pepper@infopark.de>
2019-03-26 12:23:20 +0100

    Use CopyWebpackPlugin to move static files into build folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
+
+const BUILD_PATH = path.resolve(__dirname, "build");
 
 module.exports = {
   context: path.join(__dirname, "src"),
   entry: { index: "./index" },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: "../package.json", to: BUILD_PATH },
+      { from: "../LICENSE", to: BUILD_PATH }
+    ])
+  ],
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: BUILD_PATH,
     library: "scrivito-youtube-video-widget",
     libraryTarget: "umd"
   }
