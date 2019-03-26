88f1dfdf13f1f7b642ab75a0cb57f530b5f8ae94
Alexander Pepper <alexander.pepper@infopark.de>
2019-03-26 12:41:08 +0100

    Added "mode" to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
+const webpack = require("webpack");
 
+const NODE_ENV = process.env.NODE_ENV || "production";
 const BUILD_PATH = path.resolve(__dirname, "build");
 
 module.exports = {
+  mode: NODE_ENV,
   context: path.join(__dirname, "src"),
   entry: { index: "./index" },
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env": { NODE_ENV: JSON.stringify(NODE_ENV) },
+    }),
     new CopyWebpackPlugin([
       { from: "../package.json", to: BUILD_PATH },
       { from: "../LICENSE", to: BUILD_PATH },
