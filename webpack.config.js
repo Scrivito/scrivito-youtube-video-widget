471ac6fa04a51a538951df901ec8848e312d29f2
Alexander Pepper <alexander.pepper@infopark.de>
2019-03-27 11:28:31 +0100

    [webpack] Clean up "build" before every production build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
@@ -8,51 +9,61 @@ const BUILD_PATH = path.resolve(__dirname, "build");
 const DEPENDENCIES = require("./package.json").dependencies || {};
 const PEER_DEPENDENCIES = require("./package.json").peerDependencies;
 
-module.exports = {
-  context: SRC_PATH,
-  entry: { index: "./index" },
-  output: {
-    path: BUILD_PATH,
-    library: "scrivito-youtube-video-widget",
-    libraryTarget: "umd",
-  },
-  externals: [...Object.keys(DEPENDENCIES), ...Object.keys(PEER_DEPENDENCIES)],
-  plugins: [
+module.exports = (env, argv) => {
+  const plugins = [
     new CopyWebpackPlugin([
       { from: "../package.json", to: BUILD_PATH },
       { from: "../LICENSE", to: BUILD_PATH },
       { from: "**/*.css", to: BUILD_PATH },
     ]),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [SRC_PATH],
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                "@babel/preset-react",
-                [
-                  "@babel/preset-env",
-                  {
-                    debug: false,
-                    modules: false,
-                    shippedProposals: false,
-                    useBuiltIns: false,
-                    targets: {
-                      browsers: ["Chrome >= 41", "last 2 versions"],
-                    },
-                  },
-                ],
-              ],
-              cacheDirectory: "tmp/babel-cache",
-            },
-          },
-        ],
-      },
+  ];
+  if (argv.mode === "production") {
+    plugins.unshift(new CleanWebpackPlugin());
+  }
+
+  return {
+    context: SRC_PATH,
+    entry: { index: "./index" },
+    output: {
+      path: BUILD_PATH,
+      library: "scrivito-youtube-video-widget",
+      libraryTarget: "umd",
+    },
+    externals: [
+      ...Object.keys(DEPENDENCIES),
+      ...Object.keys(PEER_DEPENDENCIES),
     ],
-  },
+    plugins,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: [SRC_PATH],
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  "@babel/preset-react",
+                  [
+                    "@babel/preset-env",
+                    {
+                      debug: false,
+                      modules: false,
+                      shippedProposals: false,
+                      useBuiltIns: false,
+                      targets: {
+                        browsers: ["Chrome >= 41", "last 2 versions"],
+                      },
+                    },
+                  ],
+                ],
+                cacheDirectory: "tmp/babel-cache",
+              },
+            },
+          ],
+        },
+      ],
+    },
+  };
 };
